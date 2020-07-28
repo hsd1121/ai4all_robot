87abfa06fcac190ad8927e55cb19225b76d5e8a7
hsd1121 <hsd1121@vt.edu>
2020-07-28 15:24:14 -0400

    updated emmanuel code

--- hector_moveit_navigation/src/emmanuel_ai4all_frontier_exploration.py
+++ hector_moveit_navigation/src/emmanuel_ai4all_frontier_exploration.py
@@ -6,8 +6,32 @@ from geometry_msgs.msg import PoseArray
 
 import hector_moveit_navigation.msg
 import octomap_msgs.msg
+import math
+VisitedVoxels= []
+#VisitedVoxels.append( [goal.position.x, goal.position.y, goal.position.z] )
+
 
 frontiers = PoseArray()
+
+
+def Vox(voxel_List,voxel_pose):
+	if voxel_List[0] ==voxel_pose.position.x and voxel_List[1] ==voxel_pose.position.y and voxel_List[2] ==voxel_pose.position.z:
+		return True
+	else:
+		return False
+
+def dista(x1, y1, z1, x2, y2, z2):  
+	d = math.sqrt(math.pow(x2 - x1, 2) +
+		math.pow(y2 - y1, 2) +
+		math.pow(z2 - z1, 2)* 1.0) 
+
+def hasVisited(current_Pose):
+	global VisitedVoxels
+	for r in VisitedVoxels:
+		if(dista(r[0], r[1], r[2],current_Pose.position.x, current_Pose.position.y,current_Pose.position.z, ))>2:
+			return False
+		else:
+			return True
 
 # Get frontiers
 def callback(data):
@@ -32,6 +56,7 @@ def navigator_client():
 	# Initialize navigation client goal message
 	goal = hector_moveit_navigation.msg.NavigationGoal()
 	rate = rospy.Rate(2)
+	global VisitedVoxels
 
 	if not rospy.is_shutdown():
 		goal.goal_pose.position.x = 0;
@@ -41,10 +66,10 @@ def navigator_client():
 		goal.goal_pose.orientation.y = 0;
 		goal.goal_pose.orientation.z = 0;
 		goal.goal_pose.orientation.w = 1;
-		
 		rospy.loginfo("Sending takeoff position (0, 0, 1)")
 		client.send_goal(goal)
 
+		VisitedVoxels.append([goal.goal_pose.position.x, goal.goal_pose.position.y, goal.goal_pose.position.z])
 		rospy.loginfo("Waiting for takeoff")
 		client.wait_for_result()
 	# Keep running while ros is okay
@@ -54,19 +79,26 @@ def navigator_client():
 
 		# Get current frontiers
 		current_frontiers = frontiers
-
 		# Navigate to all frontiers
 		''' 
-			Optimize the for loop below to more efficiently navigate to frontiers
+		Optimize the for loop below to more efficiently navigate to frontiers
 		'''
 		for pose in current_frontiers.poses:
 			goal.goal_pose = pose
+			prev_Visited = False
+
+			for num in VisitedVoxels:
+				if Vox(num,pose) or  hasVisited(pose):
+					prev_Visited = True
+
+
+			client.send_goal(goal)
 			rospy.loginfo("Sending goal position (%f, %f, %f)", pose.position.x, pose.position.y, pose.position.z)
 			rospy.loginfo("With pose (%f, %f, %f, %f)", pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
-			client.send_goal(goal)
-
 			rospy.loginfo("Waiting for result")
 			client.wait_for_result()
+			VisitedVoxels.append( [pose.position.x, pose.position.y, pose.position.z] )
+
 
 		# Sleep for half a second
 		rate.sleep()
