281f5f67032778ecd2b866dadc18d4636f4fb500
hsd1121 <hsd1121@vt.edu>
2020-07-28 15:06:53 -0400

    added individual scripts

---
+++ hector_moveit_navigation/src/megan_ai4all_frontier_exploration.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+import rospy
+import actionlib
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
+import math
+
+import hector_moveit_navigation.msg
+import octomap_msgs.msg
+
+from scipy.cluster.hierarchy import linkage
+from scipy.cluster.hierarchy import fcluster
+
+frontiers = PoseArray()
+
+
+def calculateDistance(x1,y1,z1,x2,y2,z2):
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    return dist
+
+
+# Get frontiers
+def callback(data):
+	global frontiers
+	frontiers = data
+
+def navigator_client():
+	# Initialize ROS node
+	rospy.init_node('navigator_client_py')
+	rospy.loginfo("Initialized node")
+
+	# Initialize frontier subscriber
+	rospy.Subscriber("frontiers", PoseArray, callback)
+
+	# Initialize Action Client for Navigation
+	client = actionlib.SimpleActionClient('hector_navigator', hector_moveit_navigation.msg.NavigationAction)
+
+	# Wait to connect to server
+	rospy.loginfo("Waiting for server")
+	client.wait_for_server()
+
+	# Initialize navigation client goal message
+	goal = hector_moveit_navigation.msg.NavigationGoal()
+	rate = rospy.Rate(2)
+	visiting_frontiers = PoseArray()
+
+	if not rospy.is_shutdown():
+		goal.goal_pose.position.x = 0;
+		goal.goal_pose.position.y = 0;
+		goal.goal_pose.position.z = 1;
+		goal.goal_pose.orientation.x = 0;
+		goal.goal_pose.orientation.y = 0;
+		goal.goal_pose.orientation.z = 0;
+		goal.goal_pose.orientation.w = 1;
+		
+		rospy.loginfo("Sending takeoff position (0, 0, 1)")
+		client.send_goal(goal)
+
+		rospy.loginfo("Waiting for takeoff")
+		client.wait_for_result()
+		rospy.loginfo("Done taking off")
+	# Keep running while ros is okay
+
+	while not rospy.is_shutdown():
+
+		global frontiers
+
+		# Get current frontiers
+		# rospy.loginfo("Updating frontier list")
+		current_frontiers = frontiers
+        
+		rospy.loginfo("# of frontiers: %d", len(current_frontiers.poses))
+
+		# "cluster" voxels by distance and eliminate voxels that are too close to others
+		rospy.loginfo("Converting poses to list")
+		pose_list = []
+		for pose in current_frontiers.poses:
+			pose_list.append([pose.position.x, pose.position.y, pose.position.z])
+
+		rospy.loginfo("Starting linkage")
+		Z = linkage(pose_list, method='complete', metric='euclidean')
+		rospy.loginfo("Completed linkage")
+
+		clusters = fcluster(Z, 2, criterion='distance')
+		rospy.loginfo("# of clusters: %d", len(set(clusters)))
+		'''
+		for pose in current_frontiers.poses:
+			x1 = pose.position.x
+			y1 = pose.position.y
+			z1 = pose.position.z
+			for pose2 in current_frontiers.poses:
+				x2 = pose2.position.x
+				y2 = pose2.position.y
+				z2 = pose2.position.z
+				if calculateDistance(x1,y1,z1,x2,y2,z2) >= 2:
+					visiting_frontiers.poses.append(pose2)
+
+
+		'''
+
+		''' 
+			Optimize the for loop below to more efficiently navigate to frontiers
+		'''
+		'''
+		rospy.loginfo("# of frontiers: %d", len(visiting_frontiers.poses))
+		for pose in visiting_frontiers.poses:
+			goal.goal_pose = pose
+			rospy.loginfo("Sending goal position (%f, %f, %f)", pose.position.x, pose.position.y, pose.position.z)
+			rospy.loginfo("With pose (%f, %f, %f, %f)", pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+			client.send_goal(goal)
+
+			rospy.loginfo("Waiting for result")
+			client.wait_for_result()
+		'''
+		# Sleep for half a second
+		rospy.loginfo("Sleep")
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	try:
+		navigator_client()
+	except rospy.ROSInterruptException:
+		pass
---
+++ hector_moveit_navigation/src/sahana_ai4all_frontier_exploration.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import rospy
+import actionlib
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
+import random
+
+import hector_moveit_navigation.msg
+import octomap_msgs.msg
+
+frontiers = PoseArray()#array of poses (message with position, xyz and orientation)
+
+# Get frontiers
+def callback(data):
+	global frontiers
+	frontiers = data
+
+def navigator_client():
+	# Initialize ROS node
+	rospy.init_node('navigator_client_py')
+	rospy.loginfo("Initialized node")#communication with robot
+
+	# Initialize frontier subscriber
+	rospy.Subscriber("frontiers", PoseArray, callback)
+
+	# Initialize Action Client for Navigation- telling robot where to move
+	client = actionlib.SimpleActionClient('hector_navigator', hector_moveit_navigation.msg.NavigationAction)
+
+	# Wait to connect to server
+	rospy.loginfo("Waiting for server")
+	client.wait_for_server()
+
+	# Initialize navigation client goal message- where to move robot
+	goal = hector_moveit_navigation.msg.NavigationGoal()
+	rate = rospy.Rate(2)
+
+	if not rospy.is_shutdown():
+		goal.goal_pose.position.x = 0;
+		goal.goal_pose.position.y = 0;
+		goal.goal_pose.position.z = 1;
+		goal.goal_pose.orientation.x = 0;
+		goal.goal_pose.orientation.y = 0;
+		goal.goal_pose.orientation.z = 0;
+		goal.goal_pose.orientation.w = 1;
+		
+        
+
+
+		rospy.loginfo("Sending takeoff position (0, 0, 1)")
+		client.send_goal(goal)
+
+		rospy.loginfo("Waiting for takeoff")
+		client.wait_for_result()
+	# Keep running while ros is okay
+
+	while not rospy.is_shutdown():
+		global frontiers
+
+		# Get current frontiers
+		current_frontiers = frontiers
+	        random_frontiers_list = []
+	        rospy.loginfo("Selecting 5 random poses")
+	        for i in range (0, 5):
+			number = random.randint(0, len(current_frontiers.poses)-1)
+            		while number in random_frontiers_list:
+                		number = random.randint(0, len(current_frontiers.poses)-1)
+            		random_frontiers_list.append(number)
+            
+		''' 
+			Optimize the for loop below to more efficiently navigate to frontiers
+		'''
+		rospy.loginfo("Converting random pose list to pose array")
+     		random_frontiers = PoseArray()
+		for item in random_frontiers_list:
+			rospy.loginfo("Random number: %d", item)
+			temp_pose = current_frontiers.poses[item]
+			random_frontiers.poses.append(temp_pose)
+
+	        rospy.loginfo("Random Frontier Pose Array Size: %d", len(random_frontiers.poses))
+		for pose in random_frontiers.poses:
+			goal.goal_pose = pose
+			rospy.loginfo("Sending goal position (%f, %f, %f)", pose.position.x, pose.position.y, pose.position.z)
+			rospy.loginfo("With pose (%f, %f, %f, %f)", pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+			client.send_goal(goal)
+
+			rospy.loginfo("Waiting for result")
+			client.wait_for_result()
+
+		# Sleep for half a second
+		rate.sleep()
+	
+
+	
+
+if __name__ == '__main__':
+	try:
+		navigator_client()
+	except rospy.ROSInterruptException:
+		pass
